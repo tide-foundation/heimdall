b830fdae482c7412186ca1ac1b773ebd5078ed29
Julio <julio@tide.org>
2023-03-21 16:52:43 +1000

    updated github url

--- tide.js
+++ tide.js
@@ -8,11 +8,11 @@ fetch('https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-N
     htmlDoc.getElementById("container-enclave-id").style = "background-color: transparent;" // remove background image
     var l_elements = htmlDoc.getElementsByClassName("links");
     for (var i = 0; i < l_elements.length; i++) {
-      l_elements[i].href = "https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-Node/H4x2-Node/wwwroot/" + new URL(l_elements[i].href).pathname;
+      l_elements[i].href = "https://tide-foundation.github.io/Tide-h4x2-2/H4x2-Node/H4x2-Node/wwwroot/" + new URL(l_elements[i].href).pathname;
     }
     var s_elements = htmlDoc.getElementsByClassName("s_links");
     for (var i = 0; i < s_elements.length; i++) {
-      s_elements[i].src = "https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-Node/H4x2-Node/wwwroot/" + new URL(s_elements[i].src).pathname;
+      s_elements[i].src = "https://tide-foundation.github.io/Tide-h4x2-2/H4x2-Node/H4x2-Node/wwwroot/" + new URL(s_elements[i].src).pathname;
     }
     const htmlToAdd = new XMLSerializer().serializeToString(htmlDoc);
 
