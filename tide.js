33e1b2f1551d6d167bcd31a30a97e55affa89c71
Julio <julio@tide.org>
2023-03-21 16:01:30 +1000

    added css and js full links

--- tide.js
+++ tide.js
@@ -6,6 +6,8 @@ fetch('https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-N
     const htmlDoc = parser.parseFromString(html, 'text/html');
     htmlDoc.getElementById("on-vendor").innerText = "true"; // modify element
     htmlDoc.getElementById("container-enclave-id").style = "background-color: transparent;" // remove background image
+    htmlDoc.getElementsByClassName("links").map(el => el.href = "https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-Node/H4x2-Node/wwwroot/" + el.href);
+    htmlDoc.getElementsByClassName("s_links").map(el => el.src = "https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-Node/H4x2-Node/wwwroot/" + el.src); // added full URL to fetch
     const htmlToAdd = new XMLSerializer().serializeToString(htmlDoc);
 
     // add enclave to site
