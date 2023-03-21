e00566cf45358e1429f1580e23eaa29dba61fde7
Julio <julio@tide.org>
2023-03-21 15:35:00 +1000

    now adding tide h4x enclave

--- tide.js
+++ tide.js
@@ -1,29 +1,17 @@
-import SignIn  from "https://cdn.jsdelivr.net/gh/tide-foundation/Tide-h4x2-2@master/H4x2-Node/H4x2-Node/wwwroot/modules/H4x2-TideJS/Functions/SignIn.js";
-
-async function signin() {
-  var user = document.getElementById("username").value;
-  var pass = document.getElementById("pass").value;
-
-  var config = {
-    simulatorUrl: 'https://new-simulator.australiaeast.cloudapp.azure.com/',
-    vendorUrl: 'https://h4x-staging-vendor.azurewebsites.net/'
-  } 
-  var signin = new SignIn(config);
-  var obj = await signin.return_UID_CVK(user, pass);
-
-  heimdall(obj); // vendor function!
-}
-
-fetch('https://raw.githubusercontent.com/tide-foundation/heimdall/main/tide.html')
+fetch('https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-Node/H4x2-Node/wwwroot/index.html')
   .then(response => response.text())
   .then(html => {
-    const div = document.createElement("div");
-    div.innerHTML = html;
-    div.id = "tide-div"
-    document.body.appendChild(div);
+    // modify html to modify on-vendor = true element
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(html, 'text/html');
+    htmlDoc.getElementById("on-vendor").innerText = "true"; // modify element
+    htmlDoc.getElementById("container-enclave-id").style = "background-color: transparent;" // remove background image
+    const htmlToAdd = new XMLSerializer().serializeToString(htmlDoc);
 
-    const btn = document.querySelector('.signinbtn');
-    btn.addEventListener('click', signin);
+    // add enclave to site
+    const frame = document.createElement("iframe");
+    frame.srcdoc = htmlToAdd;
+    document.body.appendChild(frame);
   });
 // do SRI checks
 
