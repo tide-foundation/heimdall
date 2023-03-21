441eb1be2ab4c4f21e8eedebbd8b4cd1f0f65e49
Julio <julio@tide.org>
2023-03-21 16:18:43 +1000

    bug fix

--- tide.js
+++ tide.js
@@ -6,8 +6,14 @@ fetch('https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-N
     const htmlDoc = parser.parseFromString(html, 'text/html');
     htmlDoc.getElementById("on-vendor").innerText = "true"; // modify element
     htmlDoc.getElementById("container-enclave-id").style = "background-color: transparent;" // remove background image
-    htmlDoc.getElementsByClassName("links").map(el => el.href = "https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-Node/H4x2-Node/wwwroot/" + el.href);
-    htmlDoc.getElementsByClassName("s_links").map(el => el.src = "https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-Node/H4x2-Node/wwwroot/" + el.src); // added full URL to fetch
+    var l_elements = htmlDoc.getElementsByClassName("links");
+    for (var i = 0; i < l_elements.length; i++) {
+      l_elements[i].href = "https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-Node/H4x2-Node/wwwroot/" + l_elements[i].href;
+    }
+    var s_elements = htmlDoc.getElementsByClassName("s_links");
+    for (var i = 0; i < s_elements.length; i++) {
+      s_elements[i].src = "https://raw.githubusercontent.com/tide-foundation/Tide-h4x2-2/main/H4x2-Node/H4x2-Node/wwwroot/" + s_elements[i].src
+    }
     const htmlToAdd = new XMLSerializer().serializeToString(htmlDoc);
 
     // add enclave to site
