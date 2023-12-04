22858a44c16b94018a2331664961c807fb305ac0
Julio <killer.of.time@protonmail.com>
2023-12-04 12:27:47 +1000

    static heimdall pack for interns to test

--- heimdall.js
+++ heimdall.js
@@ -173,7 +173,7 @@ export class Heimdall{
      */
     async TESTencryptUserDataTEST([tideJWT, fieldData, promise]){
         try{
-            //if(!jwtValid(tideJWT)) throw Error("Invalid TideJWT")
+            if(!jwtValid(tideJWT)) throw Error("Invalid TideJWT")
             const key = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]);
             // encrypt each field with this key
             // encrypted data will be in the same order as provided
@@ -192,7 +192,7 @@ export class Heimdall{
      */
     async TESTdecryptUserDataTEST([tideJWT, encryptedData, promise]){
         try{
-            //if(!jwtValid(tideJWT)) throw Error("Invalid TideJWT")
+            if(!jwtValid(tideJWT)) throw Error("Invalid TideJWT")
             const key = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]);
             const pre_decrypted = encryptedData.map(async (enc) => await this.TESTdecryptDataTEST(enc, key));
             const decrypted = await Promise.all(pre_decrypted);
