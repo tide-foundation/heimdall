219300a287dccd50fdd6445792513ce38bd51ad6
sundayScoop <killer.of.time@protonmail.com>
2025-08-07 19:19:28 +1000

    open enclave based on t.uho

--- src/enclaves/RequestEnclave.ts
+++ src/enclaves/RequestEnclave.ts
@@ -26,6 +26,8 @@ export class RequestEnclave extends Heimdall<RequestEnclave>{
         if(!data.doken) throw 'Doken not provided';
 
         this.doken = data.doken;
+        let parsedDoken = decodeToken(this.doken);
+        if(parsedDoken["t.uho"]) this.enclaveOrigin = parsedDoken["t.uho"]; // use tidecloak set user home ork from doken
         this.dokenRefreshCallback = data.dokenRefreshCallback;
         this.requireReloginCallback = data.requireReloginCallback;
 
@@ -244,6 +246,65 @@ export class RequestEnclave extends Heimdall<RequestEnclave>{
             }
         }
     }
+}
+
+function decodeToken(token: string): any {
+    const [header, payload] = token.split(".");
+
+    if (typeof payload !== "string") {
+        throw new Error("Unable to decode token, payload not found.");
+    }
+
+    let decoded;
+
+    try {
+        decoded = base64UrlDecode(payload);
+    } catch (error) {
+        throw new Error("Unable to decode token, payload is not a valid Base64URL value. Error: " + error);
+    }
+
+    try {
+        return JSON.parse(decoded);
+    } catch (error) {
+        throw new Error("Unable to decode token, payload is not a valid JSON value. Error: " + error);
+    }
+}
+
+function base64UrlDecode(input: string): string {
+    let output = input
+        .replace(/-/g, "+")
+        .replace(/_/g, "/");
+
+    switch (output.length % 4) {
+        case 0:
+            break;
+        case 2:
+            output += "==";
+            break;
+        case 3:
+            output += "=";
+            break;
+        default:
+            throw new Error("Input is not of the correct length.");
+    }
+
+    try {
+        return b64DecodeUnicode(output);
+    } catch (error) {
+        return atob(output);
+    }
+}
+
+function b64DecodeUnicode(input: string): string {
+    return decodeURIComponent(atob(input).replace(/(.)/g, (m, p) => {
+        let code = p.charCodeAt(0).toString(16).toUpperCase();
+
+        if (code.length < 2) {
+            code = "0" + code;
+        }
+
+        return "%" + code;
+    }));
 }
 
 interface decryptRequest{
