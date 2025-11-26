0483b4e7d677200b8ef495d9f9b28dfc17db33ca
sundayScoop <killer.of.time@protonmail.com>
2025-11-26 11:06:43 +1000

    updates to work with asgard

--- src/models/PolicySignRequest.ts
+++ src/models/PolicySignRequest.ts
@@ -1,10 +1,13 @@
-import { TideMemory } from "../wrapper";
-import BaseTideRequest from "./BaseTideRequest";
-import { Policy } from "./Policy";
+import { TideMemory } from "asgard-tide";
+import { BaseTideRequest } from "asgard-tide";
+import { Policy } from "asgard-tide";
 
 export default class PolicySignRequest extends BaseTideRequest {
-    constructor(policy: Policy){
-        super(
+    constructor(name: string, version: string, authFlow: string, draft: Uint8Array, dyanmicData: Uint8Array) {
+        super(name, version, authFlow, draft, dyanmicData);
+    }
+    static New(policy: Policy) : PolicySignRequest{
+        return new PolicySignRequest(
             "Policy",
             "1",
             "Policy:1",
@@ -15,25 +18,5 @@ export default class PolicySignRequest extends BaseTideRequest {
 
     getRequestedPolicy(){
         return new Policy(this.draft.GetValue(0));
-    }
-
-    static decode(data: Uint8Array): PolicySignRequest {
-        // Use parent decode to get the base request
-        const baseRequest = BaseTideRequest.decode(data);
-
-        // Extract the policy from the draft
-        const policy = new Policy(baseRequest.draft.GetValue(0));
-
-        // Create a new PolicySignRequest with the policy
-        const policyRequest = new PolicySignRequest(policy);
-
-        // Copy over all the other fields from the decoded base request
-        policyRequest.expiry = baseRequest.expiry;
-        policyRequest.authorizer = baseRequest.authorizer;
-        policyRequest.authorization = baseRequest.authorization;
-        policyRequest.authorizerCert = baseRequest.authorizerCert;
-        policyRequest.policy = baseRequest.policy;
-
-        return policyRequest;
     }
 }
